bad595f42f740ff0e7236c885a632152f369351c
Soft ware <soft8@outlook.com>
2019-03-19 18:36:03 -0300

    DONE 1

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'sql123'
+    },
+	'database': 'accesomongo',
+    'users_table': 'usuarios'
+};
